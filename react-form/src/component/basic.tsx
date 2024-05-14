200c6e1cc95d1772f8f4f5cec4bb8131b4135a80
smileJMJ <mjj0324@gmail.com>
2024-05-14 17:27:31 +0900

    ts 셋팅 및 react hook form 시작

---
+++ react-form/src/component/basic.tsx
@@ -0,0 +1,19 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+}
+
+export default function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("example"));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="test" />
+    </form>
+  );
+}
